e0f8c628c9a765e350a0a70dd788dcc1c7a51ff5
Joseph-Meiste <jomeeb2@student.oaisd.org>
2024-10-15 13:01:14 +0000

    reoganized

--- Program.cs
+++ Program.cs
@@ -4,17 +4,38 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        FindSeat();
+        SignIn();
+        DailyLogic();
+        BoxBreathing();
+        Hypotheticals();
+    }
+
+    public static void FindSeat()
+    {
         Console.WriteLine("Walk in the room.");
         Console.WriteLine("Walk towards desk.");
         Console.WriteLine("Pull out chair.");
         Console.WriteLine("Take off backpack.");
         Console.WriteLine("Place backpack on the ground.");
         Console.WriteLine("Sit down in chair.");
+    }
+
+    public static void SignIn()
+    {
         Console.WriteLine("Turn on computer.");
         Console.WriteLine("Sign in to computer.");
+    }
+
+    public static void DailyLogic()
+    {
         Console.WriteLine("Open browser.");
         Console.WriteLine("Navigate to the Daily Logic Classroom.");
         Console.WriteLine("Complete the Daily Logic for the day.");
+    }
+
+    public static void BoxBreathing()
+    {
         Console.WriteLine("Inhale for 4 seconds.");
         Console.WriteLine("Hold breath for 4 seconds.");
         Console.WriteLine("Exhale for 4 seconds.");
@@ -35,36 +56,15 @@ public class Program
         Console.WriteLine("Hold breath for 4 seconds.");
         Console.WriteLine("Exhale for 4 seconds.");
         Console.WriteLine("Hold breath for 4 seconds.");
+    }
+
+    public static void Hypotheticals()
+    {
         Console.WriteLine("Open a new tab in the browser.");
         Console.WriteLine("Navigate to Google.");
         Console.WriteLine("Open Gmail.");
         Console.WriteLine("Read Daily Hypothetical.");
         Console.WriteLine("Talk to neighbors about Daily Hypothetical.");
         Console.WriteLine("Respond to Daily Hypothetical email.");
-
-        // STEP 3.b - Uncomment the lines below after your functions have been defined
-        // FindSeat();
-        // SignIn();
-        // DailyLogic();
     }
-
-    public static void FindSeat()
-    {
-
-    }
-
-    public static void SignIn()
-    {
-
-    }
-
-    public static void DailyLogic()
-    {
-
-    }
-
-    /**
-    * Create functions for Box Breathing and Daily Hypotheticals below:
-    */
-
 }
